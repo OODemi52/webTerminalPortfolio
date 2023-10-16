737c2c9fc3920dd50e406d9867c097e1735cbf1a
OODemi52 <demidaniel98@gmail.com>
2023-10-16 15:59:08 -0400

    Removed ascii banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import './App.css'
 import Terminal from './components/Terminal'
-import AsciiBanner from './components/AsciiBanner'
 
 function App(): React.ReactNode {
 
   return (
     <>
-    <AsciiBanner />
     <div className='container'>
       <p className='message typed-out'>Welcome to my interactive terminal thememed portfolio.</p>
       <p className='message typed-out'>To learn a bit more about me, see the projects I've worked on, or to connect with me, type  
